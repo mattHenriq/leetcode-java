78a57e625ebbdca20d7ffdfb3ee81ae95cec05e4
Matheus <46304414+mattHenriq@users.noreply.github.com>
2026-07-06 11:55:49 -0300

    Update LengthOfLastWord.java to reverse traversal (O(n) time, O(1) space)

--- strings/LengthOfLastWord.java
+++ strings/LengthOfLastWord.java
@@ -1,6 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String ultimaPalavra = s.replaceAll("\\s+$", "").replaceAll(".*\\s", "");
-        return ultimaPalavra.length();
+        int i = s.length() - 1;
+        int length = 0;
+
+        // pula os espaços do final
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // conta as letras da última palavra
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
     }
 }

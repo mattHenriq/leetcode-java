4181d78c8e3562598560f78f45c6d164e68d1ae7
Matheus <46304414+mattHenriq@users.noreply.github.com>
2026-07-22 22:20:07 -0300

    Create PalindromeLinkedList.java

---
+++ linked-list/PalindromeLinkedList.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        ListNode left = head;
+        ListNode right = prev;
+        while (right != null) {
+            if (left.val != right.val) return false;
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+}
